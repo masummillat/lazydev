01e56aab27fb877e5f96ec31b5ed3c52c9487511
Masum Millat <maummillat@gmail.com>
2024-05-01 21:57:18 +0600

    configure for github page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">landing</main>;
+  return (
+    <main className="">
+      landing
+      <Image
+        src="/lazydev/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+        priority
+      />
+    </main>
+  );
 }
