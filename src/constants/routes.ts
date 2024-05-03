b2f2a202505cae21d8bb38a39c58ae7838b7bb81
Masum Millat <maummillat@gmail.com>
2024-05-04 03:14:29 +0600

    path update

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,4 @@
-import { BASE_PATH } from "./env";
-
-export const HOME_PATH = BASE_PATH + '/';
-export const ABOUT_PATH = BASE_PATH+'/about';
-export const PORTFOLIO_PATH = BASE_PATH+'/portfolio';
-export const CONTACT_PATH = BASE_PATH+'/contact';
+export const HOME_PATH =  '/';
+export const ABOUT_PATH = '/about';
+export const PORTFOLIO_PATH = '/portfolio';
+export const CONTACT_PATH = '/contact';
