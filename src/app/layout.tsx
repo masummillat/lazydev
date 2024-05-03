6368a12e1285738b0e3bac306891bfeaa951110c
Masum Millat <maummillat@gmail.com>
2024-05-04 05:10:25 +0600

    metadatabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,17 @@ import clsx from "clsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://masummillat.github.io/lazydev"),
   title: "LazyDev",
-  description: "Welcome to the lazydev",
+  description:
+    "A skilled software developer proficient in Node.js, React.js, MongoDB, and MySQL, adeptly crafts robust web applications. They architect efficient backend solutions using Node.js, harnessing its asynchronous capabilities for optimal performance. Leveraging React.js, they design dynamic user interfaces, ensuring seamless interaction and intuitive experiences. Proficient in database management, they utilize MongoDB's flexibility and scalability, alongside MySQL's relational structure, to store and retrieve data effectively. Their expertise spans from conceptualization to deployment, meticulously refining code for scalability, security, and maintainability. With a deep understanding of these technologies, they continuously innovate, delivering high-quality solutions that meet modern demands.",
+  openGraph: {
+    images: "/opengraph-image.jpeg",
+  },
+  twitter: {
+    images: "/twitter-image.jpeg",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
